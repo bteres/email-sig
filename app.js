8a28190be67b29dd74a3ac51fbedf50793d92949
Brett Terespolsky <brett.terespolsky@innovationswitch.com>
2016-10-13 22:36:38 +0200

    First commit with basic signature for Switch. Enhancements required for multiple mobile numbers, more social medias and company customisation.

---
+++ app.js
@@ -0,0 +1,21 @@
+var app = angular.module('email-sig', []);
+
+app.controller('MainCtrl', function($scope) {
+
+})
+.filter('removeSpaces', [function() {
+    return function(string) {
+        if (!angular.isString(string)) {
+            return string;
+        }
+        return string.replace(/[\s]/g, '');
+    };
+}])
+.config( [
+    '$compileProvider',
+    function( $compileProvider )
+    {   
+        $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension|tel|skype):/);
+        // Angular before v1.2 uses $compileProvider.urlSanitizationWhitelist(...)
+    }
+]);
