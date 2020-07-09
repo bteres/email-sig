e09f593362e0727a8719f75c854a815c625538ad
Brett Terespolsky <bteres@bretts-mbp.lan>
2020-07-09 11:36:25 +0200

    Added multiple mobiles

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-var app = angular.module('email-sig', []);
+var app = angular.module("email-sig", []);
 
-app.controller('MainCtrl', function($scope) {
-
-})
-.filter('removeSpaces', [function() {
-    return function(string) {
+app
+  .controller("MainCtrl", function ($scope) {
+    $scope.mobiles = [{ label: "Mobile", number: "" }];
+  })
+  .filter("removeSpaces", [
+    function () {
+      return function (string) {
         if (!angular.isString(string)) {
-            return string;
+          return string;
         }
-        return string.replace(/[\s]/g, '');
-    };
-}])
-.config( [
-    '$compileProvider',
-    function( $compileProvider )
-    {
-        $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension|tel|skype):/);
-        // Angular before v1.2 uses $compileProvider.urlSanitizationWhitelist(...)
-    }
-]);
+        return string.replace(/[\s]/g, "");
+      };
+    },
+  ])
+  .config([
+    "$compileProvider",
+    function ($compileProvider) {
+      $compileProvider.aHrefSanitizationWhitelist(
+        /^\s*(https?|ftp|mailto|chrome-extension|tel|skype):/
+      );
+      // Angular before v1.2 uses $compileProvider.urlSanitizationWhitelist(...)
+    },
+  ]);
